9850c0b6da2b251c2fd448c26712ce6e36cd637d
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-02-02 12:35:27 -0600

    first passing test

---
+++ js/overly-excited.js
@@ -0,0 +1,14 @@
+// Create an array that contains the words in the sentence
+let sentence = [];
+
+/*
+    The addExcitement function should be an impure function, and accept
+    the array as the sole argument. It should iterate over the array
+    and output the words to the browser console.
+*/
+function addExcitement (theWordArray) {
+
+}
+
+// Invoke the function and pass in the array
+addExcitement(sentence);
---
+++ test/main_test.js
@@ -0,0 +1,15 @@
+
+var expect = require('chai').expect;
+
+// Test suite
+
+describe('Mocha', function (){ // takes two arguments, a string and function
+
+// Test spec (unit test)
+
+	it("should run our tests using npm", function(){
+
+		expect(true).to.be.ok;
+	}); 
+// sanity check
+});
